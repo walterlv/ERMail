07531df77dcae176bb9c061f9d43fe9dab720df1
walterlv <xblero@gmail.com>
2018-04-15 11:37:16 +0800

    Add mail kit demo.

---
+++ src/AssembleMailing.Core/Mailing/MailClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security;
+using System.Threading.Tasks;
+using MailKit;
+using MailKit.Net.Imap;
+
+namespace Walterlv.AssembleMailing.Mailing
+{
+    public class MailClient
+    {
+        public MailClient(string host, int port, string userName, string password)
+        {
+            Host = host;
+            Port = port;
+            UserName = userName;
+            Password = new SecureString();
+            foreach (var @char in password)
+            {
+                Password.AppendChar(@char);
+            }
+        }
+
+        public string Host { get; }
+
+        public int Port { get; }
+
+        public string UserName { get; }
+
+        public SecureString Password { get; }
+
+        public async Task ConnectAsync()
+        {
+            using (var client = new ImapClient())
+            {
+                // client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                await client.ConnectAsync(Host, Port, true);
+
+                client.Authenticate(UserName, Password.ToString());
+
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+
+                Console.WriteLine("Total messages: {0}", inbox.Count);
+                Console.WriteLine("Recent messages: {0}", inbox.Recent);
+
+                for (int i = 0; i < inbox.Count; i++)
+                {
+                    var message = inbox.GetMessage(i);
+                    Console.WriteLine("Subject: {0}", message.Subject);
+                }
+
+                client.Disconnect(true);
+            }
+        }
+    }
+}
