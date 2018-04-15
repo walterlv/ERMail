d9053dcefe42fe92266be3de8d428e9204449a8f
walterlv <xblero@gmail.com>
2018-04-15 14:49:20 +0800

    Add create account dialog.

--- src/AssembleMailing.Universal/Views/ConfigMailBoxDialog.xaml.cs
+++ src/AssembleMailing.Universal/Views/ConfigMailBoxDialog.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Diagnostics.Contracts;
+using System.Globalization;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Walterlv.AssembleMailing.Mailing;
 
 // The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -24,16 +15,131 @@ namespace Walterlv.AssembleMailing.Views
             this.InitializeComponent();
         }
 
-        public string UserName { get; private set; }
+        private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs e)
+        {
+            e.Cancel = true;
+            if (!Validate()) return;
+            var deferral = e.GetDeferral();
 
-        public string Password { get; private set; }
+            var client = new MailClient(_incomingServerHost,
+                _incomingServerPort > 0 ? _incomingServerPort : 993,
+                UserName, Password);
+            await client.ConnectAsync();
+            deferral.Complete();
+        }
 
-        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs e)
         {
         }
 
-        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        public string Address
         {
+            get => _address;
+            private set => SetAndValidate(ref _address, value);
         }
+
+        public string UserName
+        {
+            get => _userName;
+            private set => SetAndValidate(ref _userName, value);
+        }
+
+        public string Password
+        {
+            get => _password;
+            private set => SetAndValidate(ref _password, value);
+        }
+
+        public string AccountName
+        {
+            get => _accountName;
+            private set => SetAndValidate(ref _accountName, value);
+        }
+
+        public string EnvelopeName
+        {
+            get => _envelopeName;
+            private set => SetAndValidate(ref _envelopeName, value);
+        }
+
+        public string IncomingServer
+        {
+            get => _incomingServerPort > 0 ? $"{_incomingServerHost}:{_incomingServerPort}" : _incomingServerHost;
+            set => SetAndValidate(out _incomingServerHost, out _incomingServerPort, value);
+        }
+
+        public string OutgoingServer
+        {
+            get => _outgoingServerPort > 0 ? $"{_outgoingServerHost}:{_outgoingServerPort}" : _outgoingServerHost;
+            set => SetAndValidate(out _outgoingServerHost, out _outgoingServerPort, value);
+        }
+
+        private static void SetHostPort(out string hostField, out int portField, string value)
+        {
+            var parts = value.Split(':', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 1)
+            {
+                hostField = parts[0].Trim();
+                portField = 0;
+            }
+            else if (parts.Length == 2 &&
+                     int.TryParse(parts[1], NumberStyles.Integer, CultureInfo.InvariantCulture, out var port))
+            {
+                hostField = parts[0].Trim();
+                portField = port;
+            }
+            else
+            {
+                hostField = null;
+                portField = 0;
+            }
+        }
+
+        private void SetAndValidate(out string hostField, out int portField, string value)
+        {
+            SetHostPort(out hostField, out portField, value);
+            Validate();
+        }
+
+        private void SetAndValidate<T>(ref T field, T value)
+        {
+            if (Equals(field, value)) return;
+
+            field = value;
+            Validate();
+        }
+
+        private bool Validate()
+        {
+            var invalid = string.IsNullOrWhiteSpace(Address)
+                          || string.IsNullOrWhiteSpace(UserName)
+                          || string.IsNullOrEmpty(Password)
+                          || string.IsNullOrWhiteSpace(AccountName)
+                          || string.IsNullOrWhiteSpace(EnvelopeName)
+                          || string.IsNullOrWhiteSpace(_incomingServerHost)
+                          || string.IsNullOrWhiteSpace(_outgoingServerHost);
+            SecondaryButtonText = invalid ? string.Empty : "Sign in";
+            return !invalid;
+        }
+
+        [ContractPublicPropertyName(nameof(Address))]
+        private string _address;
+
+        [ContractPublicPropertyName(nameof(UserName))]
+        private string _userName;
+
+        [ContractPublicPropertyName(nameof(Password))]
+        private string _password;
+
+        [ContractPublicPropertyName(nameof(AccountName))]
+        private string _accountName;
+
+        [ContractPublicPropertyName(nameof(EnvelopeName))]
+        private string _envelopeName;
+
+        private string _incomingServerHost;
+        private int _incomingServerPort;
+        private string _outgoingServerHost;
+        private int _outgoingServerPort;
     }
 }
