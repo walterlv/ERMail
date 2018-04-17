4e01fefb41d5fe22191ac0382ee57dc760ac6d9e
walterlv <xblero@gmail.com>
2018-04-17 22:48:23 +0800

    Add comment.

--- src/AssembleMailing.Core/Utils/ILogger.cs
+++ src/AssembleMailing.Core/Utils/ILogger.cs
@@ -2,8 +2,16 @@
 
 namespace Walterlv.AssembleMailing.Utils
 {
+    /// <summary>
+    /// If a class need a Logger, it should arrange an <see cref="ILogger"/> interface in it's constructor parameter list.
+    /// </summary>
     public interface ILogger
     {
+        /// <summary>
+        /// Log a new message.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="callerName"></param>
         void Log(string message, [CallerMemberName] string callerName = null);
     }
 }
