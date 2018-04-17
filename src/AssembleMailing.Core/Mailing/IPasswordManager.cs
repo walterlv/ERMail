65a95ddab9b1d341e051359a735cdb40622880e4
walterlv <xblero@gmail.com>
2018-04-17 22:46:47 +0800

    Add comment.

--- src/AssembleMailing.Core/Mailing/IPasswordManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Walterlv.AssembleMailing.Mailing
-{
-    public interface IPasswordManager
-    {
-        string Retrieve(string key);
-
-        void Add(string key, string password);
-    }
-}
