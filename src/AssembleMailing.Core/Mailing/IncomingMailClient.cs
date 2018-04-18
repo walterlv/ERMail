61f388f9300fcb21cc265cb18b210505bf7d54a8
walterlv <xblero@gmail.com>
2018-04-18 08:38:49 +0800

    Use null or notnull annotations.

--- src/AssembleMailing.Core/Mailing/IncomingMailClient.cs
+++ src/AssembleMailing.Core/Mailing/IncomingMailClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using MailKit.Net.Imap;
 using Walterlv.AssembleMailing.Models;
 
@@ -16,8 +17,9 @@ namespace Walterlv.AssembleMailing.Mailing
         /// <param name="userName"></param>
         /// <param name="password"></param>
         /// <param name="host"></param>
-        /// <param name="port"></param>
-        public IncomingMailClient(string userName, string password, string host, int port = 0)
+        /// <param name="port">Zero to use default, non-zero to specify one.</param>
+        public IncomingMailClient([NotNull] string userName, [NotNull] string password,
+            [NotNull] string host, int port = 0)
         {
             UserName = userName ?? throw new ArgumentNullException(nameof(userName));
             _password = password ?? throw new ArgumentNullException(nameof(password));
@@ -35,14 +37,17 @@ namespace Walterlv.AssembleMailing.Mailing
         /// Initialize a new instance of <see cref="IncomingMailClient"/> using the specified connection info.
         /// </summary>
         /// <param name="info"></param>
-        public IncomingMailClient(MailBoxConnectionInfo info)
-            : this(info.UserName, info.Password, info.IncomingServerHost, info.IncomingServerPort)
+        public IncomingMailClient([NotNull] MailBoxConnectionInfo info)
+            // ReSharper disable once ConstantConditionalAccessQualifier
+            : this(info?.UserName ?? throw new ArgumentNullException(nameof(info)),
+                info.Password, info.IncomingServerHost, info.IncomingServerPort)
         {
         }
 
         /// <summary>
         /// Get the incoming host. Like `example.com`.
         /// </summary>
+        [NotNull]
         public string Host { get; }
 
         /// <summary>
@@ -53,12 +58,13 @@ namespace Walterlv.AssembleMailing.Mailing
         /// <summary>
         /// Get the mail user name.
         /// </summary>
+        [NotNull]
         public string UserName { get; }
 
         /// <summary>
         /// Get the user password.
         /// </summary>
-        private readonly string _password;
+        [NotNull] private readonly string _password;
 
         /// <summary>
         /// Test connection. Just connect and then disconnect the mail server.
