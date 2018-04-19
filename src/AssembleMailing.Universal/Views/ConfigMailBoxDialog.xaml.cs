0fe6d96175b7bab6f1697e42f3e141fe896952f0
walterlv <xblero@gmail.com>
2018-04-19 19:20:35 +0800

    Add OAuth starter.

--- src/AssembleMailing.Universal/Views/ConfigMailBoxDialog.xaml.cs
+++ src/AssembleMailing.Universal/Views/ConfigMailBoxDialog.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 using MailKit.Security;
 using Walterlv.AssembleMailing.Mailing;
 using Walterlv.AssembleMailing.Models;
@@ -49,6 +52,43 @@ namespace Walterlv.AssembleMailing.Views
                 e.Cancel = true;
                 ErrorTip = ex.Message;
             }
+        }
+
+        private void AddressTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var parts = ConnectionInfo.Address.Split(new[] {'@'}, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2) return;
+
+            var mailHost = $"@{parts[1]}";
+
+            if (OAuthDictionary.TryGetValue(mailHost, out var url))
+            {
+                WebView.Visibility = Visibility.Visible;
+                WebView.Navigate(new Uri(url));
+            }
+        }
+
+        private static readonly Dictionary<string, string> OAuthDictionary = new Dictionary<string, string>
+        {
+            {"@outlook.com", "https://login.microsoftonline.com/common/oauth2/v2.0/authorize" }
+        };
+    }
+
+    internal class VisibilityReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Visibility.Visible)
+            {
+                return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
         }
     }
 }
