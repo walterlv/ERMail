97234ab3276e60dd94e579bf3e35dc6a339a7b9d
walterlv <xblero@gmail.com>
2018-04-19 21:21:17 +0800

    Add walterlv's ERMail OAuth info.

---
+++ src/ERMail.Core/OAuth/ERMailOutlookOAuth.cs
@@ -0,0 +1,11 @@
+﻿namespace Walterlv.ERMail.OAuth
+{
+    public class ERMailOutlookOAuth : IOAuthInfo
+    {
+        public string ClientId { get; } = "00000000482269A3";
+
+        public Tenant Tenant { get; } = "common";
+
+        public Scope Scope { get; } = "openid profile email";
+    }
+}
