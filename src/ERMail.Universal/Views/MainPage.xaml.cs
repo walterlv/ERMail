a05c0bb95627bc719b6648bb92ff76aad439b3f7
walterlv <xblero@gmail.com>
2018-07-07 15:58:17 +0800

    Add basic header ui.

--- src/ERMail.Universal/Views/MainPage.xaml.cs
+++ src/ERMail.Universal/Views/MainPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
+using Windows.Storage;
 using Windows.UI.Xaml.Controls;
+using Walterlv.ERMail.Models;
+using Walterlv.ERMail.Utils;
+using Walterlv.ERMail.ViewModels;
 
 namespace Walterlv.ERMail.Views
 {
@@ -9,6 +12,14 @@ namespace Walterlv.ERMail.Views
         public MainPage()
         {
             InitializeComponent();
+
+            var localFolder = ApplicationData.Current.LocalFolder;
+            _configurationFile = new FileSerializor<MailBoxConfiguration>(
+                Path.Combine(localFolder.Path, "MailBoxConfiguration.json"));
         }
+
+        private readonly FileSerializor<MailBoxConfiguration> _configurationFile;
+
+        private MainViewModel ViewModel => (MainViewModel) DataContext;
     }
 }
